b6cd5955efa2a2c7cb17a86703b5cfae24189e66
Josh Quinzon <quinzonjosh261@gmail.com>
2024-11-05 01:43:34 +0800

    Implemented UI for Number Scoreboard Component
    implemented basic layout of the numbers board (with hover effects). Still no onClick functionalities.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const ruleStack = [
     "Regal's number.",
 ];
 
-const eliminationScore = -5;
+const eliminationScore = -10;
 
 function playRound() {
   const userNum = players[0].name === "USER" ? getUserInput() : -1;
@@ -218,10 +218,78 @@ function roundIsZeroOneHundredCase(playerNumbers) {
   return false;
 }
 
-while (players.length > 1) {
-  playRound();
+// while (players.length > 1) {
+//   playRound();
+// }
+
+function displayNumbersBoard() {
+  const numbersBoard = document.createElement("div");
+
+  numbersBoard.classList.add(".numbers-board-container");
+
+  numbersBoard.style.backgroundColor = "#527569";
+  numbersBoard.style.width = "1000px";
+  numbersBoard.style.display = "grid";
+  numbersBoard.style.gridTemplateColumns = "repeat(20, 1fr)";
+  numbersBoard.style.gridTemplateRows = "repeat(5, 1fr)";
+  numbersBoard.style.border = "1px solid black";
+  numbersBoard.style.gap = "2px";
+  numbersBoard.style.padding = "2px";
+
+
+  for (let i = 0; i < 19; i++) {
+    const emptyCell = document.createElement("div");
+
+    numbersBoard.appendChild(emptyCell);
+  }
+
+  for (let i = 0; i <= 100; i++) {
+    const numberCell = document.createElement("div");
+
+    numberCell.classList.add(".number");
+    numberCell.classList.add(`#btn${i}`);
+
+    numberCell.style.height = "50px";
+    numberCell.style.border = "50px";
+    numberCell.style.backgroundColor = "white";
+    numberCell.style.display = "flex";
+    numberCell.style.alignItems = "center";
+    numberCell.style.justifyContent = "center";
+    numberCell.style.fontSize = "24px";
+    numberCell.style.color = "black";
+    numberCell.style.border = "1px solid black";
+
+    numberCell.textContent = `${i}`;
+
+    numberCell.addEventListener("mouseover", () => {
+      numberCell.style.backgroundColor = "#00090f";
+      numberCell.style.color = "white";
+      numberCell.style.cursor = "pointer";
+    });
+
+    numberCell.addEventListener("mouseout", () => {
+      numberCell.style.backgroundColor = "white";
+      numberCell.style.color = "black";
+      numberCell.style.cursor = "pointer";
+    });
+
+    numbersBoard.appendChild(numberCell);
+  }
+
+  const body = document.querySelector("body");
+
+  body.appendChild(numbersBoard);
 }
 
-console.log(
-  players.length === 1 ? `${players[0].name} WINS!!!` : "NOBODY WINS!!!"
-);
+document.addEventListener("DOMContentLoaded", () => {
+  
+  displayNumbersBoard();
+
+  // timeout template
+  // setTimeout(() => {
+  // }, 3000);
+});
+
+// console.log(
+//   players.length === 1 ? `${players[0].name} WINS!!!` : "NOBODY WINS!!!"
+// );
