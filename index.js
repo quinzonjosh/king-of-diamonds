b2bdb85f34deae21d632a398e785f9f87aa7c943
Josh Quinzon <quinzonjosh261@gmail.com>
2025-01-22 00:31:57 +0800

    Implemented Special Rules
    Special Rules are now working. Not yet tested thoroughly.

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-// last stop: fix case: 2 players remaining and the game is about to end
-
 document.addEventListener("DOMContentLoaded", () => {
   let players = [
     { name: "USER", score: 0 },
     { name: "CPU1", score: 0 },
-    { name: "CPU2", score: 0 },
-    { name: "CPU3", score: 0 },
-    { name: "CPU4", score: 0 },
+    // { name: "CPU2", score: 0 },
+    // { name: "CPU3", score: 0 },
+    // { name: "CPU4", score: 0 },
   ];
 
   let eliminationScore = -3;
@@ -18,8 +16,8 @@ document.addEventListener("DOMContentLoaded", () => {
   async function playRound(number) {
     disableNumbersBtn();
 
-    let choices = [number, ...generateRandomCPUChoices()];
-    // let choices = [...getCustomChoices()];
+    // let choices = [number, ...generateRandomCPUChoices()];
+    let choices = [...getCustomChoices()];
 
     let regalsNumber = computeRegalsNumber(choices);
 
@@ -81,34 +79,87 @@ document.addEventListener("DOMContentLoaded", () => {
     let winners = [];
     let smallestDiff = Infinity;
 
+    /******************************** SPECIAL RULES **************************/
+    // if 4 players remain, apply rule:
+    // if there are 2 people or more who chose the same number, the number
+    // becomes invalid, meaning they will lose a point even if the num
+    // is closes to the regal's number
+    if (players.length <= 4) {
+      let numberFrequency = {};
+
+      choices.forEach(
+        (number) =>
+          (numberFrequency[number] = (numberFrequency[number] || 0) + 1)
+      );
+
+      let duplicates = Object.keys(numberFrequency).filter(
+        (number) => numberFrequency[number] > 1
+      );
+
+      if (duplicates.length > 0) {
+        choices.forEach((number, index) => {
+          if (duplicates.includes(String(number))) {
+            players[index].score--;
+          }
+        });
+        return `Some players have chosen the same number!`;
+      }
+    }
+
+    // if 3 players remain, apply rule:
+    // if there's a person that chooses the exact (rounded to whole number)
+    // regal's num, the loser penalty is doubled.
+    if (players.length <= 3) {
+      if (
+        choices.some(
+          (number) => Math.round(number) === Math.round(regalsNumber)
+        )
+      ) {
+        let minChoiceIndex = choices.indexOf(Math.min(...choices));
+        players.forEach((_, index) =>
+          index !== minChoiceIndex ? (players[index].score -= 2) : null
+        );
+        return `${players[minChoiceIndex].name} has hit the Regal's Number!`;
+      }
+    }
+
+    // if 2 players remain, apply rule:
+    // if someone chooses 0, the player who chooses 100 wins the round
+    if (players.length <= 2) {
+      if (choices.includes(0) && choices.includes(100)) {
+        let losingIndex = choices.indexOf(0);
+        players[losingIndex].score--;
+        return `100 beats 0! ${players[choices.indexOf(100)].name} wins.`;
+      }
+    }
+
+    /******************************** DEFAULT RULES **************************/
     // CASE: IF ALL PLAYERS CHOSE THE SAME NUM
     if (choices.every((number) => number === choices[0])) {
       players.forEach((player) => player.score--);
       return `All Players Lose the Round!`;
     }
 
-
     // CASE: DEFAULT RULE (ONLY 1 WINS BECAUSE THEY HAVE ALL DIFF NUMS)
     choices.forEach((number, index) => {
       let difference = Math.abs(number - regalsNumber);
       if (difference < smallestDiff) {
         smallestDiff = difference;
-        winners = [index]; 
+        winners = [index];
       } else if (difference === smallestDiff) {
-        winners.push(index); 
+        winners.push(index);
       }
     });
 
     // DEDUCT POINTS
     choices.forEach((number, index) => {
       if (!winners.includes(index)) {
-        players[index].score--; 
+        players[index].score--;
       }
     });
 
     let winnerNames = winners.map((index) => players[index].name).join(", ");
     return `${winnerNames} WINS!`;
-
   }
 
   function createSimpleTextContent(data) {
@@ -279,13 +330,13 @@ document.addEventListener("DOMContentLoaded", () => {
     // return [2,4,6,8]          // 4 players no violation
     // return [12,2,4,2]         // 4 players w same num rule violation
     // return [3,55,2]           // 3 players no violation
-    // return [62,1,23]          // 3 players w 1 hitting the regals num
+    // return [62, 1, 23]; // 3 players w 1 hitting the regals num
     // return [8,24]            // 2 players no violation
-    // return [0,100]           // 2 players w 0 100 rule violation
+    return [0,100]           // 2 players w 0 100 rule violation
 
     // return [44,22,22,72,11];
     // return [27,27,27,5,90]
-    // return [30,30,30,30,5]
+    // return [30, 30, 30, 30, 5];
   }
 
   function generateRandomCPUChoices() {
